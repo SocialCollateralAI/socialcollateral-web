ed0eb11e47928814858f84a2adcc39cebe57d741
Farhan Harits Prakoso <farhan.harits05@gmail.com>
2026-01-20 01:07:05 +0700

    chore(hooks): migrate graph hooks from NetworkGraph to src/hooks
    - Move userGraphData.ts to src/hooks/useNetworkGraphData.ts (renamed)
    - Move useGraphHighlighting.ts to src/hooks/useGraphHighlighting.ts
    - Delete NetworkGraph.types.ts (replaced by src/types/)
    - Create src/hooks/index.ts barrel export
    - Update imports to use centralized types
    
    Separates reusable logic hooks from UI components folder,
    following separation of concerns principle.

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Hooks barrel export
+ */
+export { useNetworkGraphData } from './useNetworkGraphData'
+export { useGraphHighlighting } from './useGraphHighlighting'
+export { useSigmaInitialization } from './useSigmaInitialization'
